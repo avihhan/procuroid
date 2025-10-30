4bffea21ef6be4e9d32a96e4bde0e9849910e3dc
Avihhan <avihhan.official@gmail.com>
2025-10-30 11:22:50 -0400

    fixed server hosting issue

--- procuroid-frontend/src/api/apiCalls.tsx
+++ procuroid-frontend/src/api/apiCalls.tsx
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://192.168.1.236:5000/'; // change this to your API base
+const API_BASE_URL = 'http://localhost:5000/'; // change this to your API base
 
 export interface QuoteRequestPayload {
     description: string;
